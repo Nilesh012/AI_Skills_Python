eb0941d3422de2b4fc1252974a33c950861e97c1
Nilesh Waghmare <nwaghmare@microsoft.com>
2025-04-23 19:56:43 +0530

    new updates

--- labenv/Scripts/activate.bat
+++ labenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Git_Projects\AI_Skills\labenv"
+set "VIRTUAL_ENV=C:\Git_Projects\AI_Skills_Python\labenv"
 
 if not defined PROMPT set PROMPT=$P$G
 
